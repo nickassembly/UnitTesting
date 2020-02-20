25f10857181318b07def9464ca43a48a334d7993
Nick Guerra <nickg@maliciousgames.com>
2020-02-19 20:18:35 -0600

    added test project

--- DemoLibrary/Examples.cs
+++ DemoLibrary/Examples.cs
@@ -13,7 +13,7 @@ namespace DemoLibrary
             if (file.Length < 10)
             {
                 throw new ArgumentException("The file name was too short", "file");
-                //throw new System.IO.FileNotFoundException();
+               
             }
 
             return "The file was correctly loaded.";
