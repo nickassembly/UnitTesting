8e9b2eb5e77586ebdd042a7d99518924a383d465
Nick Guerra <nickg@maliciousgames.com>
2020-02-20 21:03:18 -0600

    calcuator test added

--- DemoLibrary.Tests/ExamplesTests.cs
+++ DemoLibrary.Tests/ExamplesTests.cs
@@ -11,18 +11,31 @@ namespace DemoLibrary.Tests
 {
     public class ExamplesTests
     {
-        [Fact]
-        public void ExampleLoadTextFile_ValidNameShouldWork()
-        {
-            string actual = Examples.ExampleLoadTextFile("This is a valid file name.");
+     
+      [Fact]
+      public void ExampleLoadTextFile_ValidNameShouldWork()
+      {
+         // Arrange
+         string actual = Examples.ExampleLoadTextFile("This is a valid file name.");
 
-            Assert.True(actual.Length > 0);
-        }
+         // Act
 
-        [Fact]
-        public void ExampleLoadTextFile_InvalidNameShouldFail()
-        {
-            Assert.Throws<ArgumentException>("file", () => Examples.ExampleLoadTextFile(""));
-        }
-    }
+         // Assert
+         Assert.True(actual.Length > 0);
+      }
+
+      [Fact]
+      public void ExampleLoadTextFile_InvalidNameShouldFail()
+      {
+         // Arrange
+
+         // Act
+
+         // Assert
+         Assert.Throws<ArgumentException>("file", () => Examples.ExampleLoadTextFile(""));
+       
+      }
+
+
+   }
 }
--- DemoLibrary/Examples.cs
+++ DemoLibrary/Examples.cs
@@ -6,17 +6,18 @@ using System.Threading.Tasks;
 
 namespace DemoLibrary
 {
-    public static class Examples
-    {
-        public static string ExampleLoadTextFile(string file)
-        {
-            if (file.Length < 10)
-            {
-                throw new ArgumentException("The file name was too short", "file");
-               
-            }
+   public static class Examples
+   {
+      public static string ExampleLoadTextFile(string file)
+      {
+         if (file.Length < 10)
+         {
+            throw new ArgumentException("The file name was too short", "file");
+            // throw new System.IO.FileNotFoundException();
 
-            return "The file was correctly loaded.";
-        }
-    }
+         }
+
+         return "The file was correctly loaded.";
+      }
+   }
 }
