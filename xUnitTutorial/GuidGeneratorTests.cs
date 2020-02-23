7ac4dd47bb3e82e71247f7856e3c4f1077522224
Nick Guerra <nickg@maliciousgames.com>
2020-02-22 20:24:09 -0600

    added test groups

--- xUnitTutorial/GuidGeneratorTests.cs
+++ xUnitTutorial/GuidGeneratorTests.cs
@@ -4,62 +4,66 @@ using Xunit.Abstractions;
 
 namespace xUnitTutorial
 {
-    [CollectionDefinition("Guid generator")]
-    public class GuidGeneratorDefinition : ICollectionFixture<GuidGenerator>{}
-    
-    [Collection("Guid generator")]
-    public class GuidGeneratorTestsOne
-    {
-        private readonly GuidGenerator _guidGenerator;
-        private readonly ITestOutputHelper _output;
+   [CollectionDefinition(name:"Guid generator")]
+   public class GuidGeneratorDefinition : ICollectionFixture<GuidGenerator> { }
 
-        public GuidGeneratorTestsOne(ITestOutputHelper output, 
-            GuidGenerator guidGenerator)
-        {
-            _output = output;
-            _guidGenerator = guidGenerator;
-        }
+   [Collection("Guid generator")]
+   public class GuidGeneratorTestsOne
+   {
+      private readonly GuidGenerator _guidGenerator;
+      private readonly ITestOutputHelper _output;
 
-        [Fact]
-        public void GuidTestOne()
-        {
-            var guid = _guidGenerator.RandomGuid;
-            _output.WriteLine($"The guid was: {guid}");
-        }
-        
-        [Fact]
-        public void GuidTestTwo()
-        {
-            var guid = _guidGenerator.RandomGuid;
-            _output.WriteLine($"The guid was: {guid}");
-        }
-    }
-    
-    [Collection("Guid generator")]
-    public class GuidGeneratorTestsTwo
-    {
-        private readonly GuidGenerator _guidGenerator;
-        private readonly ITestOutputHelper _output;
+      public GuidGeneratorTestsOne(ITestOutputHelper output, GuidGenerator guidGenerator)
+      {
+         _output = output;
+         _guidGenerator = guidGenerator;
+      }
 
-        public GuidGeneratorTestsTwo(ITestOutputHelper output, 
-            GuidGenerator guidGenerator)
-        {
-            _output = output;
-            _guidGenerator = guidGenerator;
-        }
 
-        [Fact]
-        public void GuidTestOne()
-        {
-            var guid = _guidGenerator.RandomGuid;
-            _output.WriteLine($"The guid was: {guid}");
-        }
-        
-        [Fact]
-        public void GuidTestTwo()
-        {
-            var guid = _guidGenerator.RandomGuid;
-            _output.WriteLine($"The guid was: {guid}");
-        }
-    }
+      [Fact]
+      public void GuidTestOne()
+      {
+         var guid = _guidGenerator.RandomGuid;
+         _output.WriteLine($"The guid was: {guid}");
+      }
+
+      [Fact]
+      public void GuidTestTwo()
+      {
+         var guid = _guidGenerator.RandomGuid;
+         _output.WriteLine($"The guid was: {guid}");
+      }
+
+   }
+
+   [Collection("Guid generator")]
+   public class GuidGeneratorTestsTwo
+   {
+      private readonly GuidGenerator _guidGenerator;
+      private readonly ITestOutputHelper _output;
+
+      public GuidGeneratorTestsTwo(ITestOutputHelper output, GuidGenerator guidGenerator)
+      {
+         _output = output;
+         _guidGenerator = guidGenerator;
+      }
+
+
+      [Fact]
+      public void GuidTestOne()
+      {
+         var guid = _guidGenerator.RandomGuid;
+         _output.WriteLine($"The guid was: {guid}");
+      }
+
+      [Fact]
+      public void GuidTestTwo()
+      {
+         var guid = _guidGenerator.RandomGuid;
+         _output.WriteLine($"The guid was: {guid}");
+      }
+
+   }
+
+
 }
