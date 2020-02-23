8a3986716e9f642787c87c2de6eb3ece13e4201f
Nick Guerra <nickg@maliciousgames.com>
2020-02-23 05:53:47 -0600

    added csv test

--- DemoWinForm/DemoLibrary.Tests/DataAccessTests.cs
+++ DemoWinForm/DemoLibrary.Tests/DataAccessTests.cs
@@ -41,5 +41,32 @@ namespace DemoLibrary.Tests
 
       }
 
+      [Fact]
+      public void ConvertModelsToCSV_ShouldWork()
+      {
+         // Arrange
+         PersonModel person1 = new PersonModel { FirstName = "Nick", LastName = "Keller" };
+         PersonModel person2 = new PersonModel { FirstName = "John", LastName = "Smith" };
+
+         List<PersonModel> people = new List<PersonModel>();
+         List<string> peopleCsv = new List<string>();
+
+         people.Add(person1);
+         people.Add(person2);
+
+         // Act
+        peopleCsv = DataAccess.ConvertModelsToCSV(people);
+
+         foreach (var person in peopleCsv)
+         {
+            Assert.IsType<string>(person);
+         }
+     
+         // Assert
+
+
+      }
+
+
    }
 }
